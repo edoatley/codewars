620ae2419e8f8ab269904256bad5ddbb26e35b52
edoatley <edoatley@gmail.com>
2020-05-25 13:13:45 +0100

    Calculator completed

--- src/main/java/org/codewars/calculator/Calculator.java
+++ src/main/java/org/codewars/calculator/Calculator.java
@@ -1,8 +1,71 @@
 package org.codewars.calculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 public class Calculator {
+    private static final List<String> operands = Arrays.asList("/*","+-");
+
     public static Double evaluate(String expression) {
-        // your code here
-        return -1.0;
+        System.err.println(expression);
+        String amendedExpression = expression;
+
+        while(amendedExpression.contains("(")) {
+            int start = amendedExpression.lastIndexOf('(');
+            int end = amendedExpression.indexOf(')', start);
+            double bracketsAnswer = evaluate(amendedExpression.substring(start + 2, end - 1)); // need to exclude the spaces
+            amendedExpression = amendedExpression.replace(amendedExpression.substring(start, end + 1), String.valueOf(bracketsAnswer));
+        }
+        System.err.println(" ->" + amendedExpression);
+        String[] elements = amendedExpression.split(" ");
+        for(String ops : operands) {
+            for (int i = 0; i < elements.length; i++) {
+                if(elements[i] != null) {
+                    if(elements[i].length() == 1 && ops.contains(elements[i].substring(0, 1))) {
+                        int prevIdx = prevIdx(i, elements);
+                        int nextIdx = nextIdx(i, elements);
+                        elements[i] = eval(Double.valueOf(elements[prevIdx]), Double.valueOf(elements[nextIdx]), elements[i].charAt(0));
+                        elements[prevIdx] = null;
+                        elements[nextIdx] = null;
+                    }
+                }
+            }
+        }
+        return Stream.of(elements).filter(Objects::nonNull).mapToDouble(Double::parseDouble).findFirst().orElse(0d);
+    }
+
+    private static int prevIdx(int start, String[] array) {
+        for (int i = start - 1; i >= 0 ; i--) {
+            if(array[i] != null) {
+                return i;
+            }
+        }
+        throw new RuntimeException(String.format("Unable to find prev index from %s in %s", start, Arrays.deepToString(array)));
+    }
+    private static int nextIdx(int start, String[] array) {
+        for (int i = start + 1; i < array.length; i++) {
+            if(array[i] != null) {
+                return i;
+            }
+        }
+
+        throw new RuntimeException(String.format("Unable to find next index from %s in %s", start, Arrays.deepToString(array)));
+    }
+
+    private static String eval(Double a, Double b, char op) {
+        switch (op) {
+            case '/':
+                return Double.toString(a / b);
+            case '*':
+                return Double.toString(a * b);
+            case '+':
+                return Double.toString(a + b);
+            case '-':
+                return Double.toString(a - b);
+            default:
+                throw new RuntimeException("Invalid operand " + op);
+        }
     }
 }
--- src/test/java/org/codewars/calculator/CalculatorTest.java
+++ src/test/java/org/codewars/calculator/CalculatorTest.java
@@ -1,8 +1,7 @@
 package org.codewars.calculator;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
@@ -30,5 +29,27 @@ public class CalculatorTest {
     @Test
     public void floats() {
         assertEquals("floats 1", new Double(0), Calculator.evaluate("7.7 - 3.3 - 4.4"), 1e-4);
+    }
+
+    @Test
+    public void brackets() {
+        assertEquals("brackets 1", new Double(1), Calculator.evaluate("7 - ( 3 * 4 ) + 6"), 1e-4);
+    }
+
+    @Test
+    public void katatest1() {
+        assertEquals("katatest 1", new Double(2 + 3 * 4 / 3 - 6 / 3 * 3 + 8), Calculator.evaluate("2 + 3 * 4 / 3 - 6 / 3 * 3 + 8"), 1e-4);
+    }
+    @Test
+    public void katatest2() {
+        assertEquals("katatest 2", new Double(2 / 2 + 3 * 4 - 6), Calculator.evaluate("2 / 2 + 3 * 4 - 6"), 1e-4);
+    }
+    @Test
+    public void katatest3() {
+        assertEquals("katatest 3", new Double(10 * 5 / 2), Calculator.evaluate("10 * 5 / 2"), 1e-4);
+    }
+    @Test
+    public void katatest4() {
+        assertEquals("katatest 4", new Double(-1306260543l + 1047780666l), Calculator.evaluate("-1306260543 + 1047780666"), 1e-4);
     }
 }
