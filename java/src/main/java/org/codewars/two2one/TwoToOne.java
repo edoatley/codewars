47af6806a18e1d6558540501c89ca2b09b3e1e00
edoatleynewday <ed.oatley@newday.co.uk>
2024-06-15 10:58:21 +0100

    save solution

--- java/src/main/java/org/codewars/two2one/TwoToOne.java
+++ java/src/main/java/org/codewars/two2one/TwoToOne.java
@@ -6,16 +6,7 @@ import java.util.stream.Stream;
 public class TwoToOne {
 
     public static String longest(String s1, String s2) {
-        String longs1 = longestStringFor(s1);
-        System.err.println("Long S1: " + longs1);
-        String longs2 = longestStringFor(s2);
-        System.err.println("Long S2: " + longs2);
-        return (longs1.length() > longs2.length()) ? longs1 : longs2;
-    }
-
-    private static String longestStringFor(String s) {
-        return s.chars()
-                .map(c -> (char) c)
+        return (s1 + s2).codePoints()
                 .distinct()
                 .sorted()
                 .collect(StringBuilder::new,
