0c3b6dd017211ed377f6602e5a26280edd371b34
edoatley <edoatley@gmail.com>
2024-07-14 16:55:16 +0100

    more progress

--- python/cutpaste/solution.py
+++ python/cutpaste/solution.py
@@ -1,4 +1,6 @@
 from typing import List
+import logging, sys
+from autologging import logged, traced, TRACE
 
 # words are groups of alphabetic chars
 # Non-alphabetic chars are considered to be punctuation and spaces (there is no distinction)
@@ -12,62 +14,70 @@ from typing import List
 # You want to remove the longest repetitions of the shortest sequences (e.g.  double in the example, not  double double))
 # Pasting occurs strictly left-to-right only appending to your current text
 
-def tokenize(text: str) -> List[str]:
-    tokens = []
-    alpha_token = text[0].isalnum()
-    current_token = ''
-
-    for i, c in enumerate(text):
-        c_alpha = c.isalnum()
-        if (alpha_token and c_alpha) or (not alpha_token and not c_alpha):
-            current_token = current_token + c
-        else:
-            tokens.append(current_token)
-            current_token = c
-            alpha_token = c_alpha
-            
-    tokens.append(current_token)
-
-    return tokens
-
-def fix_duplicate_tokens(tokens: List[str]) -> List[str]:
-    updated_tokens = []
-    # for token in tokens
-    for i, t in enumerate(tokens):
-        if not t.isalnum():
-            updated_tokens.append(t)
-            continue
-        # scan ahead for a match to the token
-        print(f'Scanning ahead for {t=}, {i=}')
-        j = [j for j, tj in enumerate(tokens[i+1:]) if t == tj]
-        # if not gound continue
-        if len(j) == 0:
-            updated_tokens.append(t)
-        else:    
-            # if found see if there is a match with the next token at each end until you find the duplicate token or they do not match
-            steps = 0
-            print(f'{j=}')
-            while (j + steps) < len(tokens) and (i + steps) < j:
-                if tokens[i+steps] == tokens[j+steps]:
-                    print(f'{tokens[i+steps]} == {tokens[j+steps]}')
-                else:
-                    steps = -1
-                    break
-                steps += 1
-            if steps > 0:
-                print(f'Potential error: {tokens[i: j+steps]}')
-                
+@logged
+@traced
+class CutPaste:
         
-    return tokens
+    def __init__(self, text : str):
+        self.__log.info("initialized")
+        self.tokens = self.tokenize(text)
+        self.errors = []
+    
+    def tokenize(self, text: str) -> List[str]:
+        tokens = []
+        alpha_token = text[0].isalnum()
+        current_token = ''
 
+        for c in text:
+            c_alpha = c.isalnum()
+            if (alpha_token and c_alpha) or (not alpha_token and not c_alpha):
+                current_token = current_token + c
+            else:
+                tokens.append(current_token)
+                current_token = c
+                alpha_token = c_alpha
+                
+        tokens.append(current_token)
 
-def fix_cut_paste(text):
-    tokens = tokenize(text)
-    print(tokens)
-    fix_duplicate_tokens(tokens)
+        return tokens
+
+    def find_duplicate_tokens(self) -> List[str]:
+        errors = []
+        # for token in tokens
+        for i, t in enumerate(self.tokens):
+            # scan ahead for a match to the token
+            dupe_indices = [index for index, token in enumerate(self.tokens[i+1:]) if t == token]
+            
+            # if none found or non-alpha token skip
+            if t.isalnum() and len(dupe_indices) > 0:   
+                # if found see if there is a match with the next token at each end until you find the duplicate token or they do not match
+                for x in dupe_indices:
+                    if self.could_be_duplicate_tokens(i, x):
+                        errors.append((i, x))
+
+        return errors
+
+    def could_be_duplicate_tokens(self, i, j):
+        if i >= j:
+            return False
+        
+        steps = 1
+        while (i + steps) < len(self.tokens) and (i + steps) < j:
+            if self.tokens[(i + steps)] != self.tokens[(j + steps)]:
+                return False
+        
+        return True
+
+    def fix_cut_paste(self):
+        self.errors = self.find_duplicate_tokens()
+        logging.info(f'Found errors: {self.errors}')
+        # logic to remove errors
+        return ''.join(self.tokens)
     
-    
-    
+        
 if __name__ == "__main__":
-    text = "Here is some piece of text piece of text that was was accidentally double double pasted."
-    fix_cut_paste(text)
+    logging.basicConfig(
+        level=TRACE, stream=sys.stderr,
+        format="%(levelname)s:%(filename)s,%(lineno)d:%(name)s.%(funcName)s:%(message)s")
+    cut_paste = CutPaste("Here is some piece of text piece of text that was was accidentally double double pasted.")
+    cut_paste.fix_cut_paste()
